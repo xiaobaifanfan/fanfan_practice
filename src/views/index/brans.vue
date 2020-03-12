dc81b5067bb33e15809534d6031f5b677632bfb6
changfan <2766843273@qq.com>
2020-03-12 18:27:16 +0800

    swiper暂存档

--- src/views/index/brans.vue
+++ src/views/index/brans.vue
@@ -19,6 +19,22 @@
     		</ul>
     	</div>
     </div>
+    <div class="new_bg">
+      <div class="left">
+          <div class="wrapper">
+              <swiper :options="swiperOption" ref="mySwiper">
+               <swiper-slide v-for='item of swiperList' :key="item.id">
+                <img :src="item.imgUrl" class="swiper-img">
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+                <div class="swiper-button-prev" slot="button-prev" @click="goprev"></div>        
+                <div class="swiper-button-next" slot="button-next" @click="gonext"></div> 
+                </swiper>
+          </div>
+
+      </div>
+      <div class="right"></div>
+    </div>
   </div>
 </template>
 
@@ -26,13 +42,40 @@
  import { swiper, swiperSlide } from 'vue-awesome-swiper';
  import { queryCategorygoods } from '../../api/api';
 export default {
-  data(){
-  return{
-  		list:[]
-  }
-  },
+  components: {
+     swiper,
+     swiperSlide,
+   },
+   data(){
+      return {
+          list:[],
+          swiperOption: {
+            pagination: { el: '.swiper-pagination',
+             clickable:true
+             },
+             loop:true,
+            autoplay:2500,
+            disableOnInteraction:false,
+            
+           
+          },
+          swiperList:[{
+      id:"0001",
+      imgUrl:"http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
+      },{
+      id:"0002",
+      imgUrl:"http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
+      }]
+      }
+   
+},
   created(){
   this.getList();
+  },
+  computed:{
+  swiper(){
+  return this.$refs.mySwiper.swiper;
+  }
   },
   methods:{
   getList(){
@@ -46,7 +89,13 @@ export default {
                 .catch(function (error) {
                   console.log(error);
                 });
-            }
+            },
+    goprev(){
+        this.swiper.slidePrev();
+    },
+    gonext(){
+        this.swiper.slideNext();
+    }
   }
 }
 </script>
@@ -60,7 +109,7 @@ div ul li {
 .brans{
 margin-top:5px;
 width:100%;
-height:auto;	
+height:auto;
 }
 .brans .ad_brans_show{
 	width:1200px;
@@ -92,5 +141,31 @@ height:auto;
 	width:100%;
 	height:100%;
 }
+.new_bg{
+  width:1200px;
+  height:auto;
+  margin-left:calc(50% - 1200px / 2);
+  background:#fff;
+  margin-top:10px;
+  margin-bottom:10px;
+}
+.new_bg .left{
+  border:1px solid red;
+  height:500px;
+  width:500px;
+  box-sizing:border-box;
+}
+
+.wrapper >>> .swiper-pagination-bullet-active
+    background:#fff;
+.wrapper
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 30.48%;
+    background: #ccc;
+    .swiper-img
+        width: 100%;
+
 </style>
 
